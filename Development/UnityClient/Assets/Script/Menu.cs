3619df592e9d80cf6663b940d9342768ee8de6f0
kawatomo <kawatomo.0331@gmail.com>
2015-04-24 00:21:19 +0900

    fix of UI transition

--- Development/UnityClient/Assets/Script/Menu.cs
+++ Development/UnityClient/Assets/Script/Menu.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class Menu : MonoBehaviour {
+
 	public void onTapButton() {
 		Debug.Log ("終了！");
 	}
---
+++ Development/UnityClient/Assets/Script/Title.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	public void OnClickTitle() {
+		Destroy(gameObject);
+		Instantiate(Resources.Load<GameObject>("Prefab/Menu"));	
+	}
+}
+
